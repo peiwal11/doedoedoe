9c2a2466d096412a9e7e97093659435c93ac8570
peiwal11 <walmarthaventbeen@gmail.com>
2024-06-27 15:29:41 +0800

    0627frevise

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd `dirname $0`
-
-REGISTRY=registry.inventec
-
-docker build --rm -f Dockerfile -t ${REGISTRY}/development/samba-test:latest .
-docker push ${REGISTRY}/development/samba-test:latest
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# set -e
-
-# 启动SSH
-# /usr/sbin/sshd
-
-python app1.py
-
